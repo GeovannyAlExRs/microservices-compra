ebbf0063999ab99972c1a2d9c47ef30a662bda4f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-17 09:08:18 -0500

    Initial commit structure of project

---
+++ src/main/java/com/company/dtk/microservicescompra/MicroservicesCompraApplication.java
@@ -0,0 +1,13 @@
+package com.company.dtk.microservicescompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesCompraApplication.class, args);
+	}
+
+}
